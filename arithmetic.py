afbd878034a089e0d683b677cc3ba5e08d271ef2
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 15:32:36 -0800

    Working functions, final exercise 2

--- arithmetic.py
+++ arithmetic.py
@@ -1,23 +1,26 @@
 def add(num1, num2):
-    return 10
+    return num1 + num2
 
 def subtract(num1, num2):
-    pass
+    return num1 - num2
 
 def multiply(num1, num2):
-    pass
+    return num1 * num2
 
 def divide(num1, num2):
-    pass
+    return float(num1) / num2
 
 def square(num1):
-    pass
+    return num1 * num1
 
 def cube(num1):
-    pass
+    return num1 ** 3
 
 def power(num1, num2):
-    pass
+    i = 1
+    for each in range(num2):
+        i *= num1
+    return i 
 
 def mod(num1, num2):
-    pass
+    return num1 % num2
